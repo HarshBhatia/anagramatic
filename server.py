0bceb7ee0a95c509514cf3ea19a9e87216d0315d
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2015-12-08 01:13:02 +0530

    enchant fix

--- server.py
+++ server.py
@@ -1,45 +1,51 @@
-from flask import Flask,render_template,request
-import enchant
+from flask import Flask, render_template, request
 import itertools
-
-d = enchant.Dict("en_US")
-
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@app.route('/solve',methods=['POST','GET'])
+
+@app.route('/solve', methods=['POST', 'GET'])
 def solver():
-	letters = request.form['letters']
-	return render_template('words.html',words = solve(letters))	
+    letters = request.form['letters']
+    return render_template('words.html', words=solve(letters))
+
 
 def solve(letters):
 
-	words = []
-	x = {}
+    words = []
+    x = {}
 
-	for i in xrange(3,len(letters)+1):
-		for e in list(itertools.combinations(letters,i)):
-			for j in list(itertools.permutations(e)):
-				if d.check(''.join(j)):
-					words.append(''.join(j))
-	b = sorted(list(set(words)),key=len)
-	b.reverse()
-	x['max_length_word'] = b[0]
+    for i in xrange(3, len(letters) + 1):
+        for e in list(itertools.combinations(letters, i)):
+            for j in list(itertools.permutations(e)):
+                if is_english_word(''.join(j)):
+                    words.append(''.join(j))
+    b = sorted(list(set(words)), key=len)
+    b.reverse()
+    x['max_length_word'] = b[0]
 
-	for ele in b:
-		if not x.get(len(ele)):
-				x[len(ele)] = [ele]
-		else:
-				x[len(ele)].append(ele)
+    for ele in b:
+        if not x.get(len(ele)):
+            x[len(ele)] = [ele]
+        else:
+            x[len(ele)].append(ele)
+
+    return x
+
+with open("wordsEn.txt") as word_file:
+    english_words = set(word.strip().lower() for word in word_file)
 
 
-	return x
+def is_english_word(word):
+    return word.lower() in english_words
+
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run(host='0.0.0.0',port =8000)
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
